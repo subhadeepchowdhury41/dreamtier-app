b17a1de3dd0b55e3783326c7b279683eeb29be46
SatendraPal <palsatyendra9984@gmail.com>
2023-10-08 13:44:57 +0530

    signup

--- lib/pages/login/signup_screen.dart
+++ lib/pages/login/signup_screen.dart
@@ -1,17 +1,18 @@
-import 'package:dreamtier/pages/login/signup_screen.dart';
+import 'package:dreamtier/pages/login/login_screen.dart';
 import 'package:dreamtier/providers/auth_provider.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_any_logo/flutter_logo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends ConsumerStatefulWidget {
-  const LoginPage({super.key});
+class SignUpScreen extends ConsumerStatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends ConsumerState<LoginPage> {
+class _LoginPageState extends ConsumerState<SignUpScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
@@ -40,6 +41,31 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                       TextFormField(
                         controller: _emailController,
                         decoration: InputDecoration(
+                          hintText: 'Username',
+                          border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  width: 1.7,
+                                  color: Color.fromARGB(255, 24, 54, 221)),
+                              borderRadius: BorderRadius.circular(7)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  width: 1.7,
+                                  color: Color.fromARGB(255, 24, 54, 221)),
+                              borderRadius: BorderRadius.circular(7)),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter username';
+                          } else if (value.length < 2) {
+                            return 'Username should be greater than 1 character';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
                           hintText: 'Email',
                           border: OutlineInputBorder(
                               borderSide: const BorderSide(
@@ -64,9 +90,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                           return null;
                         },
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const SizedBox(height: 10),
                       TextFormField(
                         obscureText: true,
                         validator: (value) {
@@ -90,12 +114,77 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                               borderRadius: BorderRadius.circular(7)),
                         ),
                       ),
+
+                      //
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                                child: Divider(color: Colors.grey.shade400)),
+                            const Text(
+                              '\t\tOR\t\t',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Expanded(
+                                child: Divider(color: Colors.grey.shade400)),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // Google, Facebook Singup
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 2.5),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                    color: Colors.blueGrey,
+                                    width: 1.0,
+                                  )),
+                              child: AnyLogo.tech.google.image(
+                                height: 42,
+                                width: 42,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 2.5),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  color: Colors.blueGrey,
+                                  width: 1.0,
+                                )),
+                            child: AnyLogo.tech.meta.image(
+                              height: 42,
+                              width: 42,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 40,
-                ),
+                const SizedBox(height: 40),
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
@@ -115,7 +204,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                       }
                     },
                     child: const Text(
-                      'Login',
+                      'SignUp',
                       style:
                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
@@ -126,7 +215,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
             Expanded(child: Container()),
             RichText(
               text: TextSpan(
-                text: 'New to our platform',
+                text: 'Already Have an Account',
                 style: const TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.w400,
@@ -134,7 +223,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                 ),
                 children: [
                   TextSpan(
-                    text: '  SignUp',
+                    text: '  LogIn',
                     style: const TextStyle(
                       fontSize: 18,
                       color: Colors.blueAccent,
@@ -144,7 +233,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                       ..onTap = () {
                         Navigator.of(context).pushReplacement(
                           MaterialPageRoute(
-                            builder: (ctx) => SignUpScreen(),
+                            builder: (ctx) => LoginPage(),
                           ),
                         );
                       },

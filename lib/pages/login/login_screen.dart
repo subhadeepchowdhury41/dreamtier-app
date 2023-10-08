18acd6eb932375da059e2707264f942ea5d01179
subhadeepchowdhury41 <subhadeepchowdhury41@gmail.com>
2023-10-08 18:04:05 +0530

    changes

--- lib/pages/login/login_screen.dart
+++ lib/pages/login/login_screen.dart
@@ -21,138 +21,140 @@ class _LoginPageState extends ConsumerState<LoginPage> {
       body: Container(
         padding: const EdgeInsets.all(13),
         margin: const EdgeInsets.symmetric(vertical: 15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/light_bg_title.png',
-                  height: MediaQuery.of(context).size.height / 3,
-                ),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      TextFormField(
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          border: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  width: 1.7,
-                                  color: Color.fromARGB(255, 24, 54, 221)),
-                              borderRadius: BorderRadius.circular(7)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  width: 1.7,
-                                  color: Color.fromARGB(255, 24, 54, 221)),
-                              borderRadius: BorderRadius.circular(7)),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter email';
-                          }
-                          final emailRegex =
-                              RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
-                          if (!emailRegex.hasMatch(value)) {
-                            return 'Please enter valid email';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter password';
-                          }
-                          return null;
-                        },
-                        controller: _passwordController,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          border: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  width: 1.7,
-                                  color: Color.fromARGB(255, 24, 54, 221)),
-                              borderRadius: BorderRadius.circular(7)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  width: 1.7,
-                                  color: Color.fromARGB(255, 24, 54, 221)),
-                              borderRadius: BorderRadius.circular(7)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(double.infinity, 50),
-                        foregroundColor: Colors.white,
-                        backgroundColor:
-                            const Color.fromARGB(255, 24, 54, 221)),
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        await ref
-                            .read(authProvider.notifier)
-                            .signInWithEmailAndPassword(
-                              _emailController.text,
-                              _passwordController.text,
-                            );
-                      }
-                    },
-                    child: const Text(
-                      'Login',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Expanded(child: Container()),
-            RichText(
-              text: TextSpan(
-                text: 'New to our platform',
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  TextSpan(
-                    text: '  SignUp',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.blueAccent,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (ctx) => SignUpScreen(),
+                  Image.asset(
+                    'assets/light_bg_title.png',
+                    height: MediaQuery.of(context).size.height / 3,
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 1.7,
+                                    color: Color.fromARGB(255, 24, 54, 221)),
+                                borderRadius: BorderRadius.circular(7)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 1.7,
+                                    color: Color.fromARGB(255, 24, 54, 221)),
+                                borderRadius: BorderRadius.circular(7)),
                           ),
-                        );
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter email';
+                            }
+                            final emailRegex = RegExp(
+                                r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+                            if (!emailRegex.hasMatch(value)) {
+                              return 'Please enter valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter password';
+                            }
+                            return null;
+                          },
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 1.7,
+                                    color: Color.fromARGB(255, 24, 54, 221)),
+                                borderRadius: BorderRadius.circular(7)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 1.7,
+                                    color: Color.fromARGB(255, 24, 54, 221)),
+                                borderRadius: BorderRadius.circular(7)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(double.infinity, 50),
+                          foregroundColor: Colors.white,
+                          backgroundColor:
+                              const Color.fromARGB(255, 24, 54, 221)),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await ref
+                              .read(authProvider.notifier)
+                              .signInWithEmailAndPassword(
+                                _emailController.text,
+                                _passwordController.text,
+                              );
+                        }
                       },
-                  )
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
-          ],
+              Expanded(child: Container()),
+              RichText(
+                text: TextSpan(
+                  text: 'New to our platform',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: '  SignUp',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.blueAccent,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (ctx) => SignUpScreen(),
+                            ),
+                          );
+                        },
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
